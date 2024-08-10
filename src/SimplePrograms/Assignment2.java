3f459ae1ac2f923cf3dfdf5e324a18a2be60bb6b
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-10 19:03:07 +0530

    Assignment2

---
+++ src/SimplePrograms/Assignment2.java
@@ -0,0 +1,9 @@
+package SimplePrograms;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+    }
+}
