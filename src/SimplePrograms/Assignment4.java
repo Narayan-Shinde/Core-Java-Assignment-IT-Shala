affeb950f3c32c53d7e697516b5339b1ba758fdf
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-10 19:19:43 +0530

    Assignment4

---
+++ src/SimplePrograms/Assignment4.java
@@ -0,0 +1,7 @@
+package SimplePrograms;
+
+public class Assignment4 {
+    public static void main(String[] args) {
+        System.out.println(2 + 4 + 5);
+    }
+}
---
+++ src/SimplePrograms/Assignment5.java
@@ -0,0 +1,8 @@
+package SimplePrograms;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        System.out.println(45 / 32);
+        System.out.println(45 * 32);
+    }
+}
