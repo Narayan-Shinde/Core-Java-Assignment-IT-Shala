091ab61f54ec95b9dadfb8ac0abe5cc8871ff034
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-10 19:22:41 +0530

    Assignment5

--- src/SimplePrograms/Assignment5.java
+++ src/SimplePrograms/Assignment5.java
@@ -2,7 +2,9 @@ package SimplePrograms;
 
 public class Assignment5 {
     public static void main(String[] args) {
+        System.out.println("Division of 45 / 32");
         System.out.println(45 / 32);
+        System.out.println("Multiplication of 45 * 32");
         System.out.println(45 * 32);
     }
 }
