fd229b814002e57f4da03fc17c486f29d5dfc406
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-11 14:35:47 +0530

    Assignment2

---
+++ src/DataTypes/Assignment2.java
@@ -0,0 +1,11 @@
+package DataTypes;
+
+public class Assignment2 {
+        public static void main(String[] args) {
+            double length = 2.0; // Length of the rectangle
+            double width = 3.0; // Width of the rectangle
+
+            double area = length * width;
+            System.out.println("Area of Rectangle is: " + area);
+        }
+    }
