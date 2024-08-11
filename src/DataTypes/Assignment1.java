6e1ddc9167e6eca4e70986d34929b54c88fb3daf
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-11 14:24:23 +0530

    Assignment1

---
+++ src/DataTypes/Assignment1.java
@@ -0,0 +1,13 @@
+package DataTypes;
+
+public class Assignment1 {
+        public static void main(String[] args) {
+            int myInt = 10;
+            double myDouble = 3.14;
+            char myChar = 'A';
+
+            System.out.println("My integer value is: " + myInt);
+            System.out.println("My double value is: " + myDouble);
+            System.out.println("My char value is: " + myChar);
+        }
+    }
