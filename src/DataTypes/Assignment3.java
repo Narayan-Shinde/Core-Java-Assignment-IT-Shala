1510b34c02b621e62f10760855f9d85a0519c926
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-13 08:08:57 +0530

    Assignment3

---
+++ src/DataTypes/Assignment3.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class Assignment3 {
+        public static void main(String[] args) {
+            double number1 = 8.2;
+            int number2 = 6;
+            double product = number1 * number2;
+            System.out.println("Product of " + number1 + " and " + number2 + " is " + product);
+        }
+    }
