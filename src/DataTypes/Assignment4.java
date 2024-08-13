71cf4332e636370e0965360c2dde3e2702fbddaf
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-13 08:14:37 +0530

    Assignment4

---
+++ src/DataTypes/Assignment4.java
@@ -0,0 +1,9 @@
+package DataTypes;
+
+public class Assignment4 {
+        public static void main(String[] args) {
+            char h ;
+
+            System.out.println((int)'h');
+        }
+    }
