8e823f0411f3435e8b84e719280bf8c0d4e676ad
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-08-13 08:29:59 +0530

    Assignment5

---
+++ src/DataTypes/Assignment5.java
@@ -0,0 +1,9 @@
+package DataTypes;
+
+public class Assignment5 {
+        public static void main(String[] args) {
+            double no=100.235 ;
+
+            System.out.println((int)no);
+        }
+    }
