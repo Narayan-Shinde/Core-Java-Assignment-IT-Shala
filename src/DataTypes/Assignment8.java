7315352ce58c41a7fc333c781b4979603f9943b7
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 11:34:50 +0530

    Assignment8

---
+++ src/DataTypes/Assignment8.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class Assignment8 {
+        public static void main(String[] args) {
+            // Declare a double variable with the value 3.9
+            double number = 3.9;
+
+            // Calculate the square of the number
+            double square = number * number;
+
+            // Print the result
+            System.out.println("The square of " + number + " is: " + square);
+        }
+
+}
