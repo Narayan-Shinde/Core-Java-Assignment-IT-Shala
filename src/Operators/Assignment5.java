001a4d90226d89c91529828d375c380bce86a354
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 17:36:03 +0530

    Assignment5

---
+++ src/Operators/Assignment5.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Assignment5 {
+        public static void main(String[] args) {
+            // Define the two numbers
+            int num1 = 23;
+            int num2 = 45;
+
+            // Check if the numbers are equal
+            if (num1 == num2) {
+                System.out.println("The numbers are equal.");
+            } else {
+                System.out.println("The numbers are not equal.");
+            }
+        }
+
+
+}
