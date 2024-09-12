df00eb1e47658e8bc6971e55d4f2b26dfaa6bf31
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 17:46:01 +0530

    Assignment8

---
+++ src/Operators/Assignment8.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Assignment8 {
+        public static void main(String[] args) {
+            // Assign values to variables
+            int a = 55;
+            int b = 70;
+
+            // Check if at least one condition is true: 'a < 50' or 'a < b'
+            if (a < 50 || a < b) {
+                System.out.println("At least one of the conditions is true.");
+            } else {
+                System.out.println("None of the conditions are true.");
+            }
+        }
+
+
+}
