6fb645f045a0a5fcc9f07d70a9c43428732f16fc
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 11:29:05 +0530

    Assignment7

---
+++ src/DataTypes/Assignment7.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public class Assignment7 {
+        public static void main(String[] args) {
+            // Declare an integer variable and a double variable
+            int intValue = 5;
+            double doubleValue = 6.2;
+
+            // Add the integer and double, store the result in a double
+            double result = intValue + doubleValue;
+
+            // Print the result
+            System.out.println("The sum of " + intValue + " and " + doubleValue + " is: " + result);
+        }
+
+}
