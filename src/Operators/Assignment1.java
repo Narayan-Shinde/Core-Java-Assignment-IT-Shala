f9ba949efecb68f606345d45f664d38176019278
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 11:40:53 +0530

    Assignment1

---
+++ src/Operators/Assignment1.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class Assignment1 {
+        public static void main(String[] args) {
+            // Declare length and breadth of the rectangle
+            int length = 5;
+            int breadth = 7;
+
+            // Calculate the area of the rectangle
+            int area = length * breadth;
+
+            // Calculate the perimeter of the rectangle
+            int perimeter = 2 * (length + breadth);
+
+            // Print the area and perimeter
+            System.out.println("The area of the rectangle is: " + area);
+            System.out.println("The perimeter of the rectangle is: " + perimeter);
+        }
+
+}
