9a64741b3c912b4130ef18a8e205ef97f27f2527
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 17:42:48 +0530

    Assignment7

---
+++ src/Operators/Assignment7.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Assignment7 {
+        public static void main(String[] args) {
+            // Assign values to variables
+            int a = 55;
+            int b = 70;
+
+            // Check if both conditions are true: 'a < 50' and 'a < b'
+            if (a < 50 && a < b) {
+                System.out.println("Both conditions are true.");
+            } else {
+                System.out.println("Both conditions are not true.");
+            }
+        }
+
+
+}
