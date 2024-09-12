f114dd81a91d401bd46eca77dd59bb8b18cdbe8c
SHINDE NARAYAN NAMDEV <shindenarayan572003@gmail.com>
2024-09-12 17:24:22 +0530

    Assignment2

---
+++ src/Operators/Assignment2.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class Assignment2 {
+        public static void main(String[] args) {
+            // Define the lengths of the sides
+            int side1 = 2;
+            int side2 = 3;
+            int side3 = 5;
+
+            // Calculate the perimeter
+            int perimeter = side1 + side2 + side3;
+
+            // Output the result
+            System.out.println("The perimeter of the triangle is: " + perimeter);
+        }
+
+}
